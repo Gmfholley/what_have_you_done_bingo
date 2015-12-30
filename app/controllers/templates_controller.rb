80de3dada9262a40ef00368370585563ece02dc0
Gwendolyn Holley <gmfholley@gmail.com>
2015-12-29 22:55:59 -0600

    Adds log in requirements to controller

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,14 +1,24 @@
 class TemplatesController < ApplicationController
+  skip_before_filter :require_login, only: [:show]
+  before_action :set_organization
   before_action :set_template, only: [:show, :edit, :update, :destroy]
+  before_action :handle_if_not_authorized, only: [:new, :create, :edit, :update, :destroy]
 
   # GET /templates/1
   # GET /templates/1.json
   def show
+    if is_admin?
+      render :show_admin
+    elsif belongs_to_organization?
+      render :show_member
+    else
+      render :show
+    end
   end
 
   # GET /templates/new
   def new
-    @template = Template.new
+    @template = Organization.templates.build
   end
 
   # GET /templates/1/edit
@@ -18,7 +28,7 @@ class TemplatesController < ApplicationController
   # POST /templates
   # POST /templates.json
   def create
-    @template = Template.new(template_params)
+    @template = Organization.templates.build(template_params)
 
     respond_to do |format|
       if @template.save
@@ -61,8 +71,13 @@ class TemplatesController < ApplicationController
       @template = Template.find(params[:id])
     end
 
+    # Use callbacks to share common setup or constraints between actions.
+    def set_organization
+      @organization = Template.find(params[:organization_id])
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def template_params
-      params.require(:template).permit(:size, :organization_id, :name, :rating, :public)
+      params.require(:template).permit(:size, :name, :rating, :is_public)
     end
 end
