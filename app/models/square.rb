eddb74c6f29c56f04422b36257fe71b4bf921f0c
Gwendolyn Holley <gmfholley@gmail.com>
2016-01-02 17:56:12 -0600

    Adds validation to models

--- app/models/square.rb
+++ app/models/square.rb
@@ -13,5 +13,30 @@
 #  updated_at  :datetime         not null
 #
 
+class SquarePositionValidator < ActiveModel::EachValidator
+ 
+  def validate_each(record, attribute, value)
+   unless record.template_id && Template.find(record.template_id).size >= value && value >= 0
+    record.errors(attribute, "cannot be less than 0 or greater than template size")
+  end
+end
+
 class Square < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with SquarePositionValidator
+  
+  validates :position_x, :square_position => true
+  validates :position_y, :square_position => true
+  validates :question, presence: true
+  validates :template, presence: true, 
+  validates_uniqueness_of :template_id, :scope => [:position_x, :position_y]
+  
+  before_save :set_default_free_space, :if => :new_record?
+
+  def set_default_free_space
+    self.free_spzce ||= false
+    return true
+  end
+  
+  
 end
