83b043926640a15b3c19bc6c1a83123548d97c09
Gwendolyn Holley <gmfholley@gmail.com>
2016-01-02 19:53:58 -0600

    Formatting change

--- app/interactors/create_squares.rb
+++ app/interactors/create_squares.rb
@@ -10,7 +10,7 @@ class CreateSquares
   def self.work(template=Template.new)
     (0...template.size).each do |x|
       (0...template.size).each do |y|
-        template.squares.build(position_x: x, position_y: y)        
+        template.squares.build(position_x: x, position_y: y)      
       end
     end
     template.squares
