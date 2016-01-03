21664af85ba4fac2498f798f78ffc24eef397e6b
Gwendolyn Holley <gmfholley@gmail.com>
2016-01-03 00:52:14 -0600

    First part of test.  Not passing.

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -22,4 +22,22 @@ class CardTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  
+  
+  test 'card should save with all variables needed' do
+    a = Card.new(template: templates(:template_one), user: users(:susan))
+    assert_not a.save, "Saved without a"
+    b = Card.new(name: "myname")
+    assert_not b.save, "Saved without an organization"
+    b.organization = organizations(:factory)
+    assert b.save, "Did not save with an organization and a factory"
+  end
+
+  
+  test 'template should generate a token after creation' do
+    a = Template.create(name: "test", organization: organizations(:factory))
+    assert_not a.token.nil?, "Created a record but did not generate a token"
+  end
+
 end
