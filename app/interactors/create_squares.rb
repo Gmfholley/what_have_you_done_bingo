bfa1dfb54bcbe201e77d691b578242aefa8629a1
Gwendolyn Holley <gmfholley@gmail.com>
2016-01-02 19:53:20 -0600

    Adds documentation

--- app/interactors/create_squares.rb
+++ app/interactors/create_squares.rb
@@ -1,6 +1,12 @@
 class CreateSquares
-
+  # intended to work with the Template class
   
+  # for a template, it will build all the square needed, with the appropriate x, y positions
+  # returns an array of Square objects
+  #
+  # template = Template objects
+  #
+  # returns an array of Square objects
   def self.work(template=Template.new)
     (0...template.size).each do |x|
       (0...template.size).each do |y|
