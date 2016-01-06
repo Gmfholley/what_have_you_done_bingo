d34bf5b1c8fa012eea09c6917e14bf566244f8cf
Gwendolyn Holley <gmfholley@gmail.com>
2016-01-05 20:44:04 -0600

    Postgres on Heroku complained about how I wrote the change of rating from string to int.  Rewritten.

--- db/migrate/20151230034958_changecolumntype.rb
+++ db/migrate/20151230034958_changecolumntype.rb
@@ -1,5 +1,5 @@
 class Changecolumntype < ActiveRecord::Migration
   def change
-    change_column :templates, :rating, :integer
+    change_column :templates, :rating, 'integer USING CAST(rating AS integer)'
   end
 end
