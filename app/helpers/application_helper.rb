c0a2b5542dccce26fe3d12c960c2e1136d93d60c
Gwendolyn Holley <gmfholley@gmail.com>
2016-01-13 00:10:10 -0600

    Wrote a few helpers for common links, date formats, and phrases used

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  
+  def humanized_time(date)
+    date.strftime("%B %d, %Y")
+  end
 end
--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,18 @@
 module CardsHelper
+  
+  def template_link(template)
+    "#{root_url}play/#{template.token}"
+  end
+  
+  def card_link(card)
+    "#{root_url}share/#{card.token}"
+  end
+  
+  def template_public_or_private_note(template)
+    if template.is_public
+      "This template is public and shareable."
+    else
+      "This template is only available to members of #{template.name}."
+    end
+  end
 end
